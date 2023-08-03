9b9297505a8ea5d1b66263c8860b277312a9312f
Jason <jasontorrealba199@gmail.com>
2023-08-03 14:08:04 -0400

    Added comments and functionality for license badge, link, and section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,32 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  } else {
+    return `[![${license} license](https://img.shields.io/badge/license-${license}-blue)](${renderLicenseLink(license)})`
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function that returns the license link
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  } else {
+    return `(https://opensource.org/licenses/${license})`
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function that returns the license section of README
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  } else {
+    return `## Licenses
+    Licensed under the ${license} license.`
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
